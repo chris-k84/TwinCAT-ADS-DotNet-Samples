50de6b920ae3a47ff3b0661209f4638c5b693ce5
Chris <chriskng84@gmail.com>
2022-11-17 21:52:22 +0000

    added 2 overloads for the CallMethodInPlc functions of the Method_Call_Samples class, the first allows a method call with no parameters, the second allows the user to provide an array of objects as parameters

--- TwinCAT-ADS-DotNet-Samples/Method-Call-Samples.cs
+++ TwinCAT-ADS-DotNet-Samples/Method-Call-Samples.cs
@@ -12,20 +12,27 @@ namespace TwinCAT_ADS_DotNet_Samples
 {
     internal class Method_Call_Samples
     {
-        void CallMethodInPLC(AmsAddress address, SessionSettings settings)
-        {
-            using (AdsSession session = new AdsSession(address, settings))
-            {
-                AdsConnection connection = (AdsConnection)session.Connect();
-
-                SymbolLoaderSettings loaderSettings = new SymbolLoaderSettings(SymbolsLoadMode.Flat);
-                ISymbolLoader loader = SymbolLoaderFactory.Create(connection, loaderSettings);
-               
-                short result = (short)connection.InvokeRpcMethod("MAIN.fbMath",
+        public void CallMethodInPLC(IAdsConnection adsConnection)
+        {               
+            short result = (short)adsConnection.InvokeRpcMethod("MAIN.fbMath",
                                                                 "mAdd",
                                                                 new object[]
                                                                 { (short)3, (short)4 });
-            }
+            
+        }
+        public void CallMethodInPLC(IAdsConnection adsConnection, string className, string methodName)
+        {               
+            short result = (short)adsConnection.InvokeRpcMethod(className,
+                                                                methodName,
+                                                                null);
+            
+        }
+
+        public void CallMethodInPLC(IAdsConnection adsConnection, string className, string methodName, object[] methodParameters )
+        {               
+            short result = (short)adsConnection.InvokeRpcMethod(className,
+                                                                methodName,methodParameters);
+            
         }
     }
 }
